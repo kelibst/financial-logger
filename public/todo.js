6b6a27b5e94813df03d68d16179a9dc9aa3347c9
Keli Booster <kbooster17@gmail.com>
2021-02-05 23:21:25 +0000

    implement delete functionality

--- public/todo.js
+++ public/todo.js
@@ -4,6 +4,7 @@ const state = document.querySelector("#state");
 const description = document.querySelector("#description");
 const ul = document.querySelector(".todo-list");
 const task = document.querySelector("#task");
+let allTask = [];
 class Todo {
     constructor(state, task, description) {
         this.state = state;
@@ -11,8 +12,7 @@ class Todo {
         this.description = description;
     }
 }
-let allTask = [];
-const renderContent = (state, task, description, container) => {
+const renderContent = (state, task, description, container, id) => {
     const li = document.createElement("li");
     const h4 = document.createElement("h4");
     h4.innerText = task;
@@ -20,34 +20,46 @@ const renderContent = (state, task, description, container) => {
     const p = document.createElement("p");
     p.innerText = description;
     li.append(p);
-    const btn = document.createElement('button');
-    btn.classList.add('btn-rmv');
-    btn.classList.add('btn');
-    btn.innerText = 'Remove';
-    btn.type = 'button';
-    const btn1 = document.createElement('button');
-    btn1.classList.add('btn-edt');
-    btn1.classList.add('btn');
-    btn1.innerText = 'Edit';
-    btn1.type = 'button';
+    const btn = document.createElement("button");
+    btn.classList.add("btn-rmv");
+    btn.classList.add("btn");
+    btn.id = `rmv-${id}`;
+    btn.innerText = "Remove";
+    btn.type = "button";
+    const btn1 = document.createElement("button");
+    btn1.classList.add("btn-edt");
+    btn1.classList.add("btn");
+    btn1.id = `edt-${id}`;
+    btn1.innerText = "Edit";
+    btn1.type = "button";
     li.append(btn1);
     li.append(btn);
     container.append(li);
 };
+ul.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("btn-rmv")) {
+        let Tasks = allTask.filter((tsk, ind) => ind !== Number(target.id.charAt(target.id.length - 1)));
+        localStorage.setItem("allTask", JSON.stringify(Tasks));
+        ul.removeChild(target.parentNode);
+    }
+});
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let allTask = [];
     const tTask = new Todo(state.value, task.value, description.value);
+    debugger;
     allTask.push(tTask);
-    localStorage.setItem('allTask', JSON.stringify(allTask));
-    renderContent(state.value, task.value, description.value, ul);
+    localStorage.setItem("allTask", JSON.stringify(allTask));
+    renderContent(state.value, task.value, description.value, ul, allTask.length - 1);
     form.reset();
 });
 window.onload = (e) => {
-    let newAllTask = localStorage.getItem('allTask');
+    let newAllTask = localStorage.getItem("allTask");
     allTask = JSON.parse(newAllTask);
     if (allTask) {
-        allTask.map(task => {
-            renderContent(task.state, task.task, task.description, ul);
+        allTask.map((task, index) => {
+            renderContent(task.state, task.task, task.description, ul, index);
         });
     }
 };
--- src/todo.ts
+++ src/todo.ts
@@ -1,26 +1,25 @@
-
 const form = document.querySelector(".new-todo-form") as HTMLFormElement;
 const state = document.querySelector("#state") as HTMLSelectElement;
 const description = document.querySelector("#description") as HTMLInputElement;
 const ul = document.querySelector(".todo-list") as HTMLUListElement;
 const task = document.querySelector("#task") as HTMLInputElement;
-
-  
-
-
-
+let allTask: Todo[] = []
 class Todo {
   constructor(
     public state: string,
     public task: string,
-    public description: string,
-  ) {
-   
-  }
+    public description: string
+  ) {}
 }
-let allTask: Todo[] = []
 
-const renderContent = (state: string, task: string, description: string, container: HTMLUListElement) =>{
+
+const renderContent = (
+  state: string,
+  task: string,
+  description: string,
+  container: HTMLUListElement,
+  id: number
+) => {
   const li = document.createElement("li");
   const h4 = document.createElement("h4");
   h4.innerText = task;
@@ -30,41 +29,59 @@ const renderContent = (state: string, task: string, description: string, contain
   p.innerText = description;
   li.append(p);
 
+  const btn = document.createElement("button");
+  btn.classList.add("btn-rmv");
+  btn.classList.add("btn");
+  btn.id = `rmv-${id}`;
 
-  const btn = document.createElement('button');
-  btn.classList.add('btn-rmv')
-  btn.classList.add('btn')
-  btn.innerText = 'Remove';
-  btn.type = 'button';
-  const btn1 = document.createElement('button');
-  btn1.classList.add('btn-edt')
-  btn1.classList.add('btn')
-  btn1.innerText = 'Edit';
-  btn1.type = 'button';
-  li.append(btn1)
-  li.append(btn)
-  container.append(li)
-}
+  btn.innerText = "Remove";
+  btn.type = "button";
+  const btn1 = document.createElement("button");
+  btn1.classList.add("btn-edt");
+  btn1.classList.add("btn");
+  btn1.id = `edt-${id}`;
+  btn1.innerText = "Edit";
+  btn1.type = "button";
+  li.append(btn1);
+  li.append(btn);
+  container.append(li);
+};
+
+ul.addEventListener("click", (e) => {
+  const target = e.target as HTMLButtonElement;
+
+  if (target.classList.contains("btn-rmv")) {
+    let Tasks = allTask.filter((tsk, ind) => ind !== Number(target.id.charAt(target.id.length - 1)))
+      
+    localStorage.setItem("allTask", JSON.stringify(Tasks));
+    ul.removeChild(target.parentNode!);
+  }
+});
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
-  
-   const tTask = new Todo(state.value, task.value, description.value)
-   allTask.push(tTask)
-   
-   localStorage.setItem('allTask', JSON.stringify(allTask))
-   renderContent(state.value, task.value, description.value, ul)
-   form.reset()
+  let allTask: Todo[] = []
+  const tTask = new Todo(state.value, task.value, description.value);
+  debugger
+  allTask.push(tTask);
+
+  localStorage.setItem("allTask", JSON.stringify(allTask));
+  renderContent(
+    state.value,
+    task.value,
+    description.value,
+    ul,
+    allTask.length - 1
+  );
+  form.reset();
 });
 
-
-
 window.onload = (e: Event) => {
- let  newAllTask = localStorage.getItem('allTask')
- allTask = JSON.parse(newAllTask!)
- if(allTask){
-   allTask.map(task => {
-     renderContent(task.state, task.task, task.description, ul)
-   })
- }
-}
+  let newAllTask = localStorage.getItem("allTask");
+  allTask = JSON.parse(newAllTask!);
+  if (allTask) {
+    allTask.map((task, index) => {
+      renderContent(task.state, task.task, task.description, ul, index);
+    });
+  }
+};
